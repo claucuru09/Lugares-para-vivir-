d54dd47100188c6ef12040ce1193d3359c58e472
claucuru09 <59249410+claucuru09@users.noreply.github.com>
2019-12-26 17:27:55 +0100

    JUEVES CAMBIOS

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,17 @@
 var time=new Date();
 var color; 
-if(time==12)
-{
-    color="verde";
-}
 
-document.getElementById("timecolor");
+if(time.getHours() ==13)
+{
+    color="green";
+}
+else if (time.getHours()== 15)
+{
+    color="red"
+}
+var ayElement = document.getElementById("timecolor");
+ayElement.innerHTML = color;
+
+
+var userName = prompt("¿Cúal es tu nombre?");
+var element2 = document.getElementById("username").innerHTML = username;
